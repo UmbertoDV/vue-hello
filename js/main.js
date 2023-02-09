9824d5e8f4f11efa6ba04bc2a6a51783eec233ef
UmbertoDV <biccoumby2002@hotmail.com>
2023-02-09 15:15:54 +0100

    add image

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const { createApp } = Vue;
+
+createApp({
+	data() {
+		return {
+			message: "Hello Vue!",
+			image: "https://picsum.photos/300/300",
+		};
+	},
+}).mount("#app");
